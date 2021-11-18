85a0fa8d206017f7469059b87d5bb40fa45b34b6
MGRINZ <24416088+MGRINZ@users.noreply.github.com>
2021-11-18 18:48:36 +0100

    Moving between rows in summary page

--- scripts/summary.js
+++ scripts/summary.js
@@ -6,6 +6,9 @@ $(function () {
 
     $("#add-ticket").click(onAddTicketClick);
 
+    $(".button-prev").click(onPrevButtonClick);
+    $(".button-next").click(onNextButtonClick);
+
     $("#pay-cash").click(() => {
         location.href = "paymentcash.html";
     });
@@ -18,14 +21,23 @@ $(function () {
 function fetchSummaryTable() {
     var tickets = JSON.parse(window.sessionStorage.getItem("tickets"));
 
-    $("#summary tr:not(.header):not(.totalPrice)").remove();
+    $("#summary tr:not(.header):not(.total-price)").remove();
 
-    var tableFooter = $("#summary .totalPrice");
+    var tableFooter = $("#summary .total-price");
 
     var totalPrice = 0;
 
     for(var i = 0; i < tickets.length; i++) {
         var row = $("<tr></tr>");
+
+        if(i >= 3) {
+            row.hide();
+            $(".button-next").css("display", "inline-flex");
+        }
+        else {
+            $(".button-prev").css("display", "none");
+            $(".button-next").css("display", "none");
+        }
         
         var name = [];
         name.push(TICKETS[tickets[i].seller].name);
@@ -40,6 +52,7 @@ function fetchSummaryTable() {
         if(tickets[i].seller == "mzkzg")
             name += "<br />" + TICKETS[tickets[i].seller][tickets[i].type].zones[tickets[i].zone].name;
     
+        row.append($("<td></td>").text(i+1));
         row.append($("<td></td>").html(name));
         row.append($("<td></td>").text(tickets[i].tickets));
         row.append($("<td></td>").text(formatPrice(tickets[i].totalPrice) + " zł"));
@@ -51,7 +64,7 @@ function fetchSummaryTable() {
         totalPrice += tickets[i].totalPrice;
     }
 
-    $(".totalPrice td:first").text(formatPrice(totalPrice) + " zł");
+    $(".total-price td:first").text(formatPrice(totalPrice) + " zł");
 
     $("#summary button.button-edit").click(onEditTicketClick);
     $("#summary button.button-remove").click(onRemoveTicketClick);
@@ -99,4 +112,32 @@ function onRemoveTicketClick(e) {
     }
 
     fetchSummaryTable();
+}
+
+function onPrevButtonClick(e) {
+    var trs = $("#summary tr:not(.header):not(.total-price)");
+    var visibleTrs = trs.filter(":visible");
+    visibleTrs.eq(visibleTrs.length - 1).hide();
+
+    var indexToShow = trs.index(visibleTrs) - 1;
+    trs.eq(indexToShow).show();
+
+    $(".button-next").css("display", "inline-flex");
+
+    if(indexToShow == 0)
+        $(this).hide();
+}
+
+function onNextButtonClick(e) {
+    var trs = $("#summary tr:not(.header):not(.total-price)");
+    var visibleTrs = trs.filter(":visible");
+    visibleTrs.eq(0).hide();
+
+    var indexToShow = trs.index(visibleTrs) + visibleTrs.length;
+    trs.eq(indexToShow).show();
+
+    $(".button-prev").css("display", "inline-flex");
+
+    if(indexToShow == trs.length - 1)
+        $(this).hide();
 }
